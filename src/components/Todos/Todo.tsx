2762f0b3a31bf9fabd1614d0f28fdd64d57a8909
jeffrey125 <jeffreyasilo8@gmail.com>
2022-06-07 01:21:46 +0800

    feat: Added Button functionality per todo

--- src/components/Todos/Todo.tsx
+++ src/components/Todos/Todo.tsx
@@ -5,7 +5,16 @@ type TodoProps = {
 };
 
 const Todo = ({ todo }: TodoProps) => {
-  return <li className={classes.listItem}>{todo}</li>;
+  return (
+    <div className={classes.container}>
+      <li className={classes.listItem}>{todo}</li>
+      <div className={classes.actions}>
+        <button>Done</button>
+        <button>Edit</button>
+        <button>Delete</button>
+      </div>
+    </div>
+  );
 };
 
 export default Todo;
