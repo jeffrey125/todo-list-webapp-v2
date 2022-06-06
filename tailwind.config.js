b0ae6c55c4b041f17faf500b79076844cadb12fd
jeffrey125 <jeffreyasilo8@gmail.com>
2022-06-07 02:08:20 +0800

    feat: Migrated to Tailwind CSS

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        rubik: ['Rubik', 'sans-serif'],
+      },
+      colors: {
+        primaryColor: '#3F88c5',
+        palette1: '#44bba4',
+        palette1Shade: '#308373',
+        palette2: '#e94f37',
+        fontGray: '#393e41',
+      },
+    },
+  },
+  plugins: [],
+};
