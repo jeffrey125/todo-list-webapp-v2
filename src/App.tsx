b6c7bc48525493e7cc9c551a20eb8a5f39adc2e8
jeffrey125 <jeffreyasilo8@gmail.com>
2022-06-07 19:07:14 +0800

    style: Added Dark mode

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,28 @@
 import { useState } from 'react';
 
-import { TodosObj } from './model/TodosType';
+import { TodosObj, AddTodo } from './Types/TodosType';
 import NewTodo from './components/NewTodo/NewTodo';
 import Todos from './components/Todos/Todos';
 import Card from './components/UI/Card';
+import MainBG from './components/UI/MainBG';
 
 function App() {
   const [todosArr, setTodosArr] = useState<TodosObj[]>([]);
 
-  const addTodo = (todo: TodosObj) => {
+  const addTodo: AddTodo = (todo) => {
     setTodosArr((state) => [...state, todo]);
   };
 
   return (
-    <main className="flex justify-center items-center w-screen h-screen">
+    <MainBG>
       <Card>
+        {/* <button className="bg-palette1 p-2 mb-2 rounded-xl w-1/2 place-self-center">
+          Toggle Dark Mode
+        </button> */}
         <Todos items={todosArr} />
         <NewTodo onAddTodo={addTodo} />
       </Card>
-    </main>
+    </MainBG>
   );
 }
 
