d3f31abaada7944de7d6d254069a42e817e12427
jeffrey125 <jeffreyasilo8@gmail.com>
2022-06-10 02:04:55 +0800

    feat: added a reusable mobile submit button

---
+++ src/components/UI/MobileEditSaveBTN.tsx
@@ -0,0 +1,27 @@
+import { ReactNode, FormEvent } from 'react';
+
+interface MobileEditSaveBTNProps {
+  handler: (e: FormEvent) => void;
+  children: ReactNode;
+  className?: string;
+  disableButton?: boolean;
+}
+
+const MobileEditSaveBTN = ({
+  handler,
+  children,
+  disableButton,
+  className,
+}: MobileEditSaveBTNProps) => {
+  return (
+    <button
+      onClick={handler}
+      className={`group ${className} flex justify-center items-center border-2 border-solid border-palette1 h-10 w-10 sm:h-12 sm:w-12 rounded-full bg-palette1 hover:bg-palette1Shade hover:shadow  focus:bg-palette1Shade focus:shadow active:bg-palette1Shade active:shadow disabled:bg-slate-600 disabled:cursor-not-allowed`}
+      disabled={disableButton}
+    >
+      {children}
+    </button>
+  );
+};
+
+export default MobileEditSaveBTN;
