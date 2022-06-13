1fa7faae2d8592fc73d106338ca06522611c9007
jeffrey125 <jeffreyasilo8@gmail.com>
2022-06-13 19:42:47 +0800

    feat: Added lazy loading

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,63 @@
-import { useRef } from 'react';
+import { AnimatePresence, motion } from 'framer-motion';
+import React, { useRef, useContext, useEffect, Suspense } from 'react';
 
-import NewTodo from './components/NewTodo/NewTodo';
-import Todos from './components/Todos/Todos';
-import Card from './components/UI/Card';
-import MainBG from './components/UI/MainBG';
-import ThemeSwitcher from './components/UI/ThemeSwitcher';
+import PageLoader from './components/UI/PageLoader';
+import ThemeContext from './store/theme-context';
+const Card = React.lazy(() => import('./components/UI/Card'));
+const MainBG = React.lazy(() => import('./components/UI/MainBG'));
+const ThemeSwitcher = React.lazy(() => import('./components/UI/ThemeSwitcher'));
+const Todos = React.lazy(() => import('./components/Todos/Todos'));
+const NewTodo = React.lazy(() => import('./components/NewTodo/NewTodo'));
+
+const containerVariant = {
+  visible: {
+    opacity: 1,
+    transition: {
+      duration: 0.5,
+    },
+    hidden: {
+      opacity: 0,
+    },
+  },
+};
 
 function App() {
   const divRef = useRef<HTMLDivElement>(null);
+  const themeCtx = useContext(ThemeContext);
+  const { theme, isDarkMode } = themeCtx;
+
+  useEffect(() => {
+    const root = window.document.documentElement;
+
+    if (isDarkMode) {
+      root.classList.remove('light');
+      root.classList.add(theme);
+    } else {
+      root.classList.remove('dark');
+      root.classList.add(theme);
+    }
+  }, [theme, isDarkMode]);
 
   return (
-    <MainBG>
-      <Card>
-        <ThemeSwitcher />
-        <Todos dummyDiv={divRef} />
-        <NewTodo dummyDiv={divRef} />
-      </Card>
-    </MainBG>
+    <AnimatePresence exitBeforeEnter>
+      <Suspense fallback={<PageLoader />}>
+        <motion.main
+          key="main-content"
+          animate="visible"
+          initial="hidden"
+          exit="hidden"
+          variants={containerVariant}
+        >
+          <MainBG>
+            <Card>
+              <ThemeSwitcher />
+              <Todos dummyDiv={divRef} />
+              <NewTodo dummyDiv={divRef} />
+            </Card>
+          </MainBG>
+        </motion.main>
+      </Suspense>
+    </AnimatePresence>
   );
 }
 
